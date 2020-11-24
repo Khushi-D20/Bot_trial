7a5a042d059dde1ebc0c12c4f059afc824495029
Khushi-D20 <dhingra.khushiyan@gmail.com>
2020-11-24 19:16:25 +0530

    bruh

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -1,4 +1,4 @@
 module.exports = (client, member) => {
-  const defaultChannel = member.guild.channels.cache.find(channel => channel.permissionsFor(guild.me).has("SEND_MESSAGES"));
+  const defaultChannel = member.guild.channels.cache.find(channel => channel.name == 'general');
   defaultChannel.send(`Welcome ${member.user} to this server, kindly prepare yourself for Khushi freaking out over small things, and the rest sitting back and watching! And if you don't like pizza and ice cream, you may as well leave now.`).catch(console.error);
 }
