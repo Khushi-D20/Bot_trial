69692265236a749a72af8b7e6a15178ce2d15edf
Khushi-D20 <dhingra.khushiyan@gmail.com>
2020-12-03 19:49:19 +0530

    this

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const Discord = require("discord.js");
 const Enmap = require("enmap");
 const fs = require("fs");
+const ytdl = require('ytdl-core');
 
 const client = new Discord.Client();
 const config = require("./config.json");
 // We also need to make sure we're attaching the config to the CLIENT so it's accessible everywhere!
 client.config = config;
+
 
 fs.readdir("./events/", (err, files) => {
   if (err) return console.error(err);
